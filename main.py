c65d5c5b91956103b34c9ede13af30c2f0a643ed
ZachTheArcticFox <thenichollsfamily159389@outlook.com>
2025-05-21 11:58:19 +0100

    Added select tool

--- main.py
+++ main.py
@@ -2,57 +2,53 @@ import pygame, sys, time
 
 class Block:
     def __init__(self, type_, pos):
-        self.type = type_  # block type
-        self.pos = pos  # block position
-        self.state = False  # block state
-        self.next_state = False  # next block state
+        self.type = type_
+        self.pos = pos
+        self.state = False
+        self.next_state = False
         match self.type:
             case 'NOT':
-                self.colour_on = (255, 0, 0)  # red
+                self.colour_on = (255, 0, 0)
             case 'AND':
-                self.colour_on = (0, 0, 255)  # blue
+                self.colour_on = (0, 0, 255)
             case 'XOR':
-                self.colour_on = (127, 0, 255)  # purple
+                self.colour_on = (127, 0, 255)
             case 'OR':
-                self.colour_on = (0, 255, 0)  # green
+                self.colour_on = (0, 255, 0)
             case 'TFLIPFLOP':
-                self.colour_on = (60, 60, 60)  # black
+                self.colour_on = (60, 60, 60)
             case 'NODE':
-                self.colour_on = (255, 255, 255)  # white
-        self.colour_off = tuple(max(int(c * 0.6), 0) for c in self.colour_on)  # darkens colour_on by 40%
-        self.rect = (pygame.Rect(self.pos[0], self.pos[1], 25, 25), self.colour_off)  # creates rect to render
-        self.inputs = [] # blocks that are connected to this block
+                self.colour_on = (255, 255, 255)
+        self.colour_off = tuple(max(int(c * 0.6), 0) for c in self.colour_on)
+        self.rect = (pygame.Rect(self.pos[0], self.pos[1], 25, 25), self.colour_off)
+        self.inputs = []
 
     def update_rect(self):
         self.rect = (pygame.Rect(self.pos[0], self.pos[1], 25, 25),
-                     self.colour_on if self.state else self.colour_off)  # updates rendered rect to turn on and off
+                     self.colour_on if self.state else self.colour_off)
 
-blocks = [] # where the blocks are stored
-mode = 'build' # mode (build, wire, etc.)
-block = 'NOT' # selected block
-tps = 20 # ticks / second
-
-# the blocks for the wiring system
+blocks = []
+mode = 'build'
+block = 'NOT'
+tps = 20
 wre_1 = None
 wre_2 = None
+selecting = False
+select_start = None
+select_end = None
+selected_blocks = []
 
 print("""Controls:
 1 through 6: Change block
-Ctrl+1 through Ctrl+4: Change tool
-0: Print state and next state for all blocks""")
+Ctrl+1 through Ctrl+5: Change tool
+0: Print state and next state for all blocks
+Del: Delete selection""")
 
-def update_blockswires():  # updates the rect for all blocks
+def update_blockswires():
     for i in blocks:
         i.update_rect()
 
-idxconv = [
-    'NOT',
-    'AND',
-    'XOR',
-    'OR',
-    'TFLIPFLOP',
-    'NODE'
-]
+idxconv = ['NOT', 'AND', 'XOR', 'OR', 'TFLIPFLOP', 'NODE']
 
 def import_from_file(fp):
     global blocks
@@ -88,18 +84,16 @@ def export_to_file(fp):
         fw.write(output)
     return output
 
-pygame.init() # initialize pygame
-
-screen = pygame.display.set_mode((pygame.display.Info().current_w * 0.75, pygame.display.Info().current_h * 0.75)) # screen surface
-pygame.display.set_caption('Pygame Circuit Maker') # changes window name
-clock = pygame.time.Clock() # pygame clock
+pygame.init()
+screen = pygame.display.set_mode((pygame.display.Info().current_w * 0.75, pygame.display.Info().current_h * 0.75))
+pygame.display.set_caption('Pygame Circuit Maker')
+clock = pygame.time.Clock()
 
 import_from_file('main.save')
 
 def tick():
     global blocks, tps
-    time.sleep(1/tps) # tps delay
-
+    time.sleep(1 / tps)
     for idx, blk in enumerate(blocks):
         inputs = [inp.state for inp in blk.inputs]
         match blk.type:
@@ -113,7 +107,6 @@ def tick():
                 blk.next_state = sum(inputs) % 2 == 1 if inputs else False
             case 'NODE':
                 blk.next_state = inputs[0] if inputs else False
-        
     for blk in blocks:
         blk.state = blk.next_state
 
@@ -122,6 +115,7 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit(0)
+
         if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
             mx, my = pygame.mouse.get_pos()
             print(f'click at: {mx}, {my}')
@@ -152,6 +146,27 @@ while True:
                     if i.rect[0].collidepoint(event.pos):
                         if i.type == 'TFLIPFLOP':
                             i.next_state = not i.state
+            elif mode == 'select':
+                selecting = True
+                select_start = event.pos
+                select_end = event.pos
+
+        elif event.type == pygame.MOUSEBUTTONUP and event.button == 1:
+            if selecting and mode == 'select':
+                selecting = False
+                select_end = event.pos
+                selected_blocks.clear()
+                x1, y1 = select_start
+                x2, y2 = select_end
+                selection_rect = pygame.Rect(min(x1, x2), min(y1, y2), abs(x2 - x1), abs(y2 - y1))
+                for blk in blocks:
+                    if selection_rect.colliderect(blk.rect[0]):
+                        selected_blocks.append(blk)
+                print(f"selected {len(selected_blocks)} blocks.")
+
+        elif event.type == pygame.MOUSEMOTION and selecting and mode == 'select':
+            select_end = event.pos
+
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_1:
                 block = 'NOT'
@@ -162,7 +177,7 @@ while True:
             if event.key == pygame.K_4:
                 block = 'OR'
             if event.key == pygame.K_5:
-                block = 'TFLIPFLOP' 
+                block = 'TFLIPFLOP'
             if event.key == pygame.K_6:
                 block = 'NODE'
             if pygame.key.get_pressed()[pygame.K_LCTRL] or pygame.key.get_pressed()[pygame.K_RCTRL]:
@@ -178,18 +193,33 @@ while True:
                 if event.key == pygame.K_4:
                     mode = 'interact'
                     print(mode)
+                if event.key == pygame.K_5:
+                    mode = 'select'
+                    print(mode)
             if event.key == pygame.K_0:
                 for i in blocks:
                     print(i.next_state, i.state)
+            if event.key == pygame.K_DELETE:
+                for i in selected_blocks:
+                    blocks.remove(i)
 
     screen.fill((0, 0, 0))
     tick()
-    #import_from_file('main.save')
     export_to_file('main.save')
+
     for i in blocks:
         pygame.draw.rect(screen, i.rect[1], i.rect[0])
+        if i in selected_blocks:
+            pygame.draw.rect(screen, (255, 255, 0), i.rect[0], 2)
         i.update_rect()
         for j in i.inputs:
-            pygame.draw.line(screen, (255, 255, 255), i.pos, (j.pos[0]+25, j.pos[1]))
+            pygame.draw.line(screen, (255, 255, 255), i.pos, (j.pos[0] + 25, j.pos[1]))
+
+    if selecting and select_start and select_end and mode == 'select':
+        x1, y1 = select_start
+        x2, y2 = select_end
+        rect = pygame.Rect(min(x1, x2), min(y1, y2), abs(x2 - x1), abs(y2 - y1))
+        pygame.draw.rect(screen, (0, 255, 0), rect, 1)
+
     pygame.display.flip()
     clock.tick(60)
